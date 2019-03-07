af0087e4db6a5604088dbf148a69ae57097b74d4
RifaniAditya <48306021+RifaniAditya@users.noreply.github.com>
2019-03-07 10:52:47 +0700

    Add files via upload

---
+++ rifani.java
@@ -0,0 +1,7 @@
+public class rifani {
+    public static void main(String[] args) {
+        System.out.println("Nama  : Rifani Aditiya\n");
+        System.out.println("NIM   : 17090050\n");
+        System.out.println("Kelas : TI 4A");
+    }
+}
